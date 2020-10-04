2d4a5fafdbddcad225a1b79459712d5943b7b008
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-10-04 16:45:36 +0530

    Sum of two Numbers

---
+++ Practice/Program1.c
@@ -0,0 +1,13 @@
+// Sum of two Numbers
+
+#include<stdio.h>
+int main(){
+
+    int a,b, sum;
+
+    printf("Enter Two Numbers : ");
+    scanf("%d%d",&a,&b);
+
+    sum=a+b;
+    printf("Sum = %d",sum);
+}
